6a71002b713be1872f2450a2f32e7e977f00f412
Alexsandro <alexsandrorodriguess@gmail.com>
2019-02-14 00:44:44 -0300

    Implementação da tela principal

--- AgendaNet/wwwroot/js/mainController.js
+++ AgendaNet/wwwroot/js/mainController.js
@@ -1,3 +1,15 @@
-﻿agendaNet.controller('MainController', ['$scope', 'GlobalConfig', function ($scope, GlobalConfig) {
+﻿agendaNet.controller('MainController', ['$scope', 'GlobalConfig', 'AgendaApiService', function ($scope, GlobalConfig, AgendaApiService) {
     $scope.appName = GlobalConfig.appName;
+    $scope.contatos = [];
+
+    $scope.listarContatos = function () {
+        AgendaApiService.listarContatos().then(function success(response) {
+            $scope.contatos = response.data;
+        });
+    };
+
+    (function init() {
+        $scope.listarContatos();
+    })();
+
 }]);
---
+++ AgendaNet/wwwroot/js/services/agendaApiService.js
@@ -0,0 +1,25 @@
+﻿agendaNet.factory('AgendaApiService', ['$http', 'GlobalConfig', function ($http, GlobalConfig) {
+
+    return AgendaApiService = {
+
+        listarContatos: function () {
+            return $http.get(GlobalConfig.apiBaseUrl);
+        },
+
+        getContatoPorId: function (id) {
+            return $http.get(GlobalConfig.apiBaseUrl + '/' + id);
+        },
+
+        salvarContato: function (contato) {
+            return $http.post(GlobalConfig.apiBaseUrl, contato);
+        },
+
+        alterarContato: function (contato) {
+            return $http.put(GlobalConfig.apiBaseUrl, contato);
+        },
+
+        excluirContato: function (id) {
+            return $http.delete(GlobalConfig.apiBaseUrl + '/' + id);
+        }
+    };
+}]);
